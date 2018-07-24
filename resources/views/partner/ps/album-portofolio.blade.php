54e38ade1d63caa382e13dd95ce66819d50c1f3d
adi darmawan <badutdarmawan1711@gmail.com>
2018-07-24 20:08:53 +0700

    asjd

--- resources/views/partner/ps/album-portofolio.blade.php
+++ resources/views/partner/ps/album-portofolio.blade.php
@@ -14,25 +14,25 @@
                          <form role="form" action="{{ route('partner.upload.portofolio') }}" method="post" enctype="multipart/form-data">
                     {{ csrf_field() }}
                             <div class="row">
-                                <div class="col-md-3">
+                                <div class="col-md-3 col-sm-6">
                                     <label>Upload Portofolio Terbaik 1</label>
                                     <div class="file-loading">
                                         <input id="file-0a" class="file" type="file" name="album_img_1">
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-3 col-sm-6">
                                     <label>Upload Portofolio Terbaik 2</label>
                                     <div class="file-loading">
                                         <input id="file-0a" class="file" type="file" name="album_img_2">
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-3 col-sm-6">
                                     <label>Upload Portofolio Terbaik 3</label>
                                     <div class="file-loading">
                                         <input id="file-0a" class="file" type="file" name="album_img_3">
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-3 col-sm-6">
                                     <label>Upload Portofolio Terbaik 4</label>
                                     <div class="file-loading">
                                         <input id="file-0a" class="file" type="file" name="album_img_4">
@@ -50,17 +50,17 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <table>
-                                    <tbody>
+                                    <tbody style="text-align: center;">
                                         <tr>
-                                            <td>
+                                            <td >
                                             @if(!empty($data->album_img_1))
-                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;" class="img-responsive" src="{{ asset('album/'.$data->album_img_1.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
+                                            <img style="text-align: center; align-content: center; width: 500px; height: auto; padding: 20px;" class="img-responsive" src="{{ asset('album/'.$data->album_img_1.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
                                             @else
                                             @endif    
                                             </td>
                                             <td>
                                             @if(!empty($data->album_img_2))
-                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;" class="img-responsive" src="{{ asset('album/'.$data->album_img_2.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
+                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;padding: 20px;" class="img-responsive" src="{{ asset('album/'.$data->album_img_2.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
                                             @else
                                             @endif
                                             </td>
@@ -68,13 +68,13 @@
                                         <tr>
                                             <td>
                                             @if(!empty($data->album_img_3))
-                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;" class="img-responsive" src="{{ asset('album/'.$data->album_img_3.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
+                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;padding: 20px;" class="img-responsive" src="{{ asset('album/'.$data->album_img_3.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
                                             @else
                                             @endif    
                                             </td>
                                             <td>
                                             @if(!empty($data->album_img_4))
-                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;" class="img-responsive" src="{{ asset('album/'.$data->album_img_4.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
+                                            <img style="text-align: center; align-content: center; width: 500px; height: auto;padding: 20px;" class="img-responsive" src="{{ asset('album/'.$data->album_img_4.'.jpg')  }}" alt= "Album / Portofolio Bisnis" />
                                             @else
                                             @endif   
                                             </td>
